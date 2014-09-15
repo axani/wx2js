2fca16f8d3272dfce00da7b231152bbea3a35591
Filip Axani <filip.axani@gmail.com>
2014-09-15 16:29:15 +0200

    Send text from input field to html element

--- MainFrame.py
+++ MainFrame.py
@@ -8,10 +8,15 @@ class MainFrame(gui.wx2jsWindow):
 		# Create window frame
 		# All gui-stuff is in gui.py
 		gui.wx2jsWindow.__init__(self, parent)
+
+		# Set Value for Input field
+		self.wxInputField.SetValue("Hallo")
+
 		print wx.version()
 		print 'App loaded'
 
 	# Run this function when button is pressed
 	def sendVarToJS(self, event):
-		self.htmlPanel.browser.RunScript('document.body.style.background = "red";')
+		js_function = 'document.getElementById("variable").innerHTML = "%s";' % self.wxInputField.GetValue()
+		self.htmlPanel.browser.RunScript(js_function)
 		print 'Button clicked', self.wxInputField.GetValue()
